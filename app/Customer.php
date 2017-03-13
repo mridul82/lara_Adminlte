26e1e3add9714ebe70b0e0c2a82fce66668b4998
mridul82 <mridulesh1@gmail.com>
2017-03-13 20:56:37 +0530

    first commit

---
+++ app/Customer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    protected $fillable = [
+        'user_id', 'cust_firstname', 'cust_lastname', 'cust_city', 'cust_no'
+    ];
+
+    public function user()
+    {
+        return $this->hasOne('App\User');
+    }
+}
---
+++ database/migrations/2017_02_26_101954_create_customers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('cust_firstname');
+            $table->string('cust_lastname');
+            $table->text('cust_city');
+            $table->string('cust_no');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
